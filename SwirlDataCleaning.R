f289be637cdd8ae3517a3675fcf0df71b1578d0e
aselsmeyer <aselsmeyer@yahoo.com>
2016-05-27 11:21:28 -0500

    Finished assign 1& 2 of Getting and Cleaning Data in Swirl

--- SwirlDataCleaning.R
+++ SwirlDataCleaning.R
@@ -998,3 +998,537 @@ package count unique countries  avg_bytes
 
 > exit()
 Error: could not find function "exit"
+| Hi! I see that you have some variables saved in your workspace. To keep things running smoothly, I recommend you clean up before starting
+| swirl.
+
+| Type ls() to see a list of the variables in your workspace. Then, type rm(list=ls()) to clear your workspace.
+
+| Type swirl() when you are ready to begin.
+
+Warning message:
+package ‘swirl’ was built under R version 3.2.5 
+> swirl()
+
+| Welcome to swirl! Please sign in. If you've been here before, use the same name as you did then. If you are new, call yourself something
+| unique.
+
+What shall I call you? Alyx
+
+| Would you like to continue with one of these lessons?
+
+1: Getting and Cleaning Data Grouping and Chaining with dplyr
+2: No. Let me start something new.
+
+Selection: 1
+
+| Attempting to load lesson dependencies...
+
+| Package ‘dplyr’ loaded correctly!
+  
+  
+  
+  | Now we can isolate only those packages which had more than 679 total downloads. Use filter() to select all rows from pack_sum for which
+| 'count' is strictly greater (>) than 679. Store the result in a new variable called top_counts.
+
+> top_counts <- filter(pack_sum, count > 679)
+
+| Perseverance, that's the answer.
+
+|================================================                                                                 |  42%
+
+| Let's take a look at top_counts. Print it to the console.
+
+> top_counts
+Source: local data frame [61 x 5]
+
+package count unique countries   avg_bytes
+(chr) (int)  (int)     (int)       (dbl)
+1      bitops  1549   1408        76   28715.046
+2         car  1008    837        64 1229122.307
+3     caTools   812    699        64  176589.018
+4  colorspace  1683   1433        80  357411.197
+5  data.table   680    564        59 1252721.215
+6         DBI  2599    492        48  206933.250
+7    devtools   769    560        55  212932.640
+8   dichromat  1486   1257        74  134731.938
+9      digest  2210   1894        83  120549.294
+10     doSNOW   740     75        24    8363.755
+..        ...   ...    ...       ...         ...
+
+| Keep up the great work!
+  
+  |==================================================                                                               |  44%
+
+| There are only 61 packages in our top 1%, so we'd like to see all of them. Since dplyr only shows us the first 10 rows, we can use the
+| View() function to see more.
+
+...
+
+|====================================================                                                             |  46%
+
+| View all 61 rows with View(top_counts). Note that the 'V' in View() is capitalized.
+
+> View(top_counts)
+
+| Keep working like that and you'll get there!
+  
+  |======================================================                                                           |  48%
+
+| arrange() the rows of top_counts based on the 'count' column and assign the result to a new variable called top_counts_sorted. We want
+| the packages with the highest number of downloads at the top, which means we want 'count' to be in descending order. If you need help,
+| check out ?arrange and/or ?desc.
+
+> top_counts_soted <- arrange(top_counts, count)
+
+| Not quite, but you're learning! Try again. Or, type info() for more options.
+
+| arrange(top_counts, desc(count)) will arrange the rows of top_counts based on the values of the 'count' variable, in
+| descending order. Don't forget to assign the result to top_counts_sorted.
+
+> top_counts_sorted <- arrange(top_counts, desc(count))
+
+| That's correct!
+
+|========================================================                                                         |  50%
+
+| Now use View() again to see all 61 rows of top_counts_sorted.
+
+> view(top_counts_sorted)
+Error: could not find function "view"
+> View(top_counts_sorted)
+
+| You are quite good my friend!
+
+|===========================================================                                                      |  52%
+
+| If we use total number of downloads as our metric for popularity, then the above output shows us the most popular
+| packages downloaded from the RStudio CRAN mirror on July 8, 2014. Not surprisingly, ggplot2 leads the pack with 4602
+| downloads, followed by Rcpp, plyr, rJava, ....
+
+...
+
+|=============================================================                                                    |  54%
+
+| ...And if you keep on going, you'll see swirl at number 43, with 820 total downloads. Sweet!
+  
+  ...
+
+|===============================================================                                                  |  56%
+
+| Perhaps we're more interested in the number of *unique* downloads on this particular day. In other words, if a package
+| is downloaded ten times in one day from the same computer, we may wish to count that as only one download. That's what
+| the 'unique' column will tell us.
+
+...
+
+|=================================================================                                                |  58%
+
+| Like we did with 'count', let's find the 0.99, or 99%, quantile for the 'unique' variable with quantile(pack_sum$unique,
+| probs = 0.99).
+
+> quantile(pack_sum$unique, probs = 0.99)
+99% 
+465 
+
+| Excellent job!
+
+|===================================================================                                              |  60%
+
+| Apply filter() to pack_sum to select all rows corresponding to values of 'unique' that are strictly greater than 465.
+| Assign the result to a variable called top_unique.
+
+> top_unique <- filter(pack_sum, unique > 465)
+
+| You got it right!
+
+|======================================================================                                           |  62%
+
+| Let's View() our top contenders!
+  
+  > view(top_unique)
+Error: could not find function "view"
+> View(top_unique)
+
+| Nice work!
+  
+  |========================================================================                                         |  63%
+
+| Now arrange() top_unique by the 'unique' column, in descending order, to see which packages were downloaded from the
+| greatest number of unique IP addresses. Assign the result to top_unique_sorted.
+
+> top_unique_sorted <- arrange(top_unique, desc(unique))
+
+| You got it right!
+  
+  |==========================================================================                                       |  65%
+
+| View() the sorted data.
+
+> View(top_unique_sorted)
+
+| Excellent work!
+  
+  |============================================================================                                     |  67%
+
+| Now Rcpp is in the lead, followed by stringr, digest, plyr, and ggplot2. swirl moved up a few spaces to number 40, with
+| 698 unique downloads. Nice!
+  
+  ...
+
+|==============================================================================                                   |  69%
+
+| Our final metric of popularity is the number of distinct countries from which each package was downloaded. We'll
+| approach this one a little differently to introduce you to a method called 'chaining' (or 'piping').
+
+...
+
+|================================================================================                                 |  71%
+
+| Chaining allows you to string together multiple function calls in a way that is compact and readable, while still
+| accomplishing the desired result. To make it more concrete, let's compute our last popularity metric from scratch,
+| starting with our original data.
+
+...
+
+|===================================================================================                              |  73%
+
+| I've opened up a script that contains code similar to what you've seen so far. Don't change anything. Just study it for
+| a minute, make sure you understand everything that's there, then submit() when you are ready to move on.
+
+Error in editor(file = file, title = title) : 
+  argument "name" is missing, with no default
+
+| Leaving swirl now. Type swirl() to resume.
+
+> options(editor = "internal")
+> swirl()
+
+| Welcome to swirl! Please sign in. If you've been here before, use the same name as you did then. If you are new, call
+| yourself something unique.
+
+What shall I call you? Alyx
+
+| Would you like to continue with one of these lessons?
+
+1: Getting and Cleaning Data Grouping and Chaining with dplyr
+2: No. Let me start something new.
+
+Selection: 1
+
+| Attempting to load lesson dependencies...
+
+| Package ‘dplyr’ loaded correctly!
+
+
+
+| I've opened up a script that contains code similar to what you've seen so far. Don't change anything. Just study it for
+| a minute, make sure you understand everything that's there, then submit() when you are ready to move on.
+# Don't change any of the code below. Just type submit()
+# when you think you understand it.
+
+# We've already done this part, but we're repeating it
+# here for clarity.
+
+by_package <- group_by(cran, package)
+pack_sum <- summarize(by_package,
+                      count = n(),
+                      unique = n_distinct(ip_id),
+                      countries = n_distinct(country),
+                      avg_bytes = mean(size))
+
+# Here's the new bit, but using the same approach we've
+# been using this whole time.
+
+top_countries <- filter(pack_sum, countries > 60)
+result1 <- arrange(top_countries, desc(countries), avg_bytes)
+
+# Print the results to the console.
+print(result1)
+
+> submit()
+
+| Sourcing your script...
+
+Source: local data frame [46 x 5]
+
+package count unique countries  avg_bytes
+(chr) (int)  (int)     (int)      (dbl)
+1          Rcpp  3195   2044        84 2512100.35
+2        digest  2210   1894        83  120549.29
+3       stringr  2267   1948        82   65277.17
+4          plyr  2908   1754        81  799122.79
+5       ggplot2  4602   1680        81 2427716.05
+6    colorspace  1683   1433        80  357411.20
+7  RColorBrewer  1890   1584        79   22763.99
+8        scales  1726   1408        77  126819.33
+9        bitops  1549   1408        76   28715.05
+10     reshape2  2032   1652        76  330128.26
+..          ...   ...    ...       ...        ...
+
+| That's correct!
+
+|=====================================================================================                            |  75%
+
+| It's worth noting that we sorted primarily by country, but used avg_bytes (in ascending order) as a tie breaker. This
+| means that if two packages were downloaded from the same number of countries, the package with a smaller average
+| download size received a higher ranking.
+
+...
+
+|=======================================================================================                          |  77%
+
+| We'd like to accomplish the same result as the last script, but avoid saving our intermediate results. This requires
+| embedding function calls within one another.
+
+...
+
+|=========================================================================================                        |  79%
+
+| That's exactly what we've done in this script. The result is equivalent, but the code is much less readable and some of
+| the arguments are far away from the function to which they belong. Again, just try to understand what is going on here,
+| then submit() when you are ready to see a better solution.
+
+# Don't change any of the code below. Just type submit()
+# when you think you understand it. If you find it
+# confusing, you're absolutely right!
+
+result2 <-
+  arrange(
+    filter(
+      summarize(
+        group_by(cran,
+                 package
+        ),
+        count = n(),
+        unique = n_distinct(ip_id),
+        countries = n_distinct(country),
+        avg_bytes = mean(size)
+      ),
+      countries > 60
+    ),
+    desc(countries),
+    avg_bytes
+  )
+
+print(result2)
+
+> submit()
+
+| Sourcing your script...
+
+Source: local data frame [46 x 5]
+
+package count unique countries  avg_bytes
+(chr) (int)  (int)     (int)      (dbl)
+1          Rcpp  3195   2044        84 2512100.35
+2        digest  2210   1894        83  120549.29
+3       stringr  2267   1948        82   65277.17
+4          plyr  2908   1754        81  799122.79
+5       ggplot2  4602   1680        81 2427716.05
+6    colorspace  1683   1433        80  357411.20
+7  RColorBrewer  1890   1584        79   22763.99
+8        scales  1726   1408        77  126819.33
+9        bitops  1549   1408        76   28715.05
+10     reshape2  2032   1652        76  330128.26
+..          ...   ...    ...       ...        ...
+
+| Your dedication is inspiring!
+  
+  |===========================================================================================                      |  81%
+
+| In this script, we've used a special chaining operator, %>%, which was originally introduced in the magrittr R package
+| and has now become a key component of dplyr. You can pull up the related documentation with ?chain. The benefit of %>%
+| is that it allows us to chain the function calls in a linear fashion. The code to the right of %>% operates on the
+| result from the code to the left of %>%.
+| 
+| Once again, just try to understand the code, then type submit() to continue.
+
+# Read the code below, but don't change anything. As
+# you read it, you can pronounce the %>% operator as
+# the word 'then'.
+#
+# Type submit() when you think you understand
+# everything here.
+
+result3 <-
+  cran %>%
+  group_by(package) %>%
+  summarize(count = n(),
+            unique = n_distinct(ip_id),
+            countries = n_distinct(country),
+            avg_bytes = mean(size)
+  ) %>%
+  filter(countries > 60) %>%
+  arrange(desc(countries), avg_bytes)
+
+# Print result to console
+print(result3)
+
+> submit()
+
+| Sourcing your script...
+
+Source: local data frame [46 x 5]
+
+package count unique countries  avg_bytes
+(chr) (int)  (int)     (int)      (dbl)
+1          Rcpp  3195   2044        84 2512100.35
+2        digest  2210   1894        83  120549.29
+3       stringr  2267   1948        82   65277.17
+4          plyr  2908   1754        81  799122.79
+5       ggplot2  4602   1680        81 2427716.05
+6    colorspace  1683   1433        80  357411.20
+7  RColorBrewer  1890   1584        79   22763.99
+8        scales  1726   1408        77  126819.33
+9        bitops  1549   1408        76   28715.05
+10     reshape2  2032   1652        76  330128.26
+..          ...   ...    ...       ...        ...
+
+| All that hard work is paying off!
+  
+  |=============================================================================================                    |  83%
+
+| So, the results of the last three scripts are all identical. But, the third script provides a convenient and concise
+| alternative to the more traditional method that we've taken previously, which involves saving results as we go along.
+
+...
+
+|================================================================================================                 |  85%
+
+| Once again, let's View() the full data, which has been stored in result3.
+
+> View(result3)
+
+| Perseverance, that's the answer.
+
+|==================================================================================================               |  87%
+
+| It looks like Rcpp is on top with downloads from 84 different countries, followed by digest, stringr, plyr, and ggplot2.
+| swirl jumped up the rankings again, this time to 27th.
+
+...
+
+|====================================================================================================             |  88%
+
+| To help drive the point home, let's work through a few more examples of chaining.
+
+...
+
+|======================================================================================================           |  90%
+
+| Let's build a chain of dplyr commands one step at a time, starting with the script I just opened for you.
+
+# select() the following columns from cran. Keep in mind
+# that when you're using the chaining operator, you don't
+# need to specify the name of the data tbl in your call to
+# select().
+#
+# 1. ip_id
+# 2. country
+# 3. package
+# 4. size
+#
+# The call to print() at the end of the chain is optional,
+# but necessary if you want your results printed to the
+# console. Note that since there are no additional arguments
+# to print(), you can leave off the parentheses after
+# the function name. This is a convenient feature of the %>%
+# operator.
+
+cran %>%
+  select() %>%
+	print
+
+cran %>%
+  select(ip_id, country, package, size) %>%
+  print
+
+> submit()
+
+| Sourcing your script...
+
+Source: local data frame [225,468 x 4]
+
+ip_id country      package    size
+(int)   (chr)        (chr)   (int)
+1      1      US    htmltools   80589
+2      2      US      tseries  321767
+3      3      US        party  748063
+4      3      US        Hmisc  606104
+5      4      CA       digest   79825
+6      3      US randomForest   77681
+7      3      US         plyr  393754
+8      5      US      whisker   28216
+9      6      CN         Rcpp    5928
+10     7      US     hflights 2206029
+..   ...     ...          ...     ...
+
+| You are really on a roll!
+
+|========================================================================================================         |  92%
+
+| Let's add to the chain.
+
+# Use mutate() to add a column called size_mb that contains
+# the size of each download in megabytes (i.e. size / 2^20).
+#
+# If you want your results printed to the console, add
+# print to the end of your chain.
+
+cran %>%
+  select(ip_id, country, package, size) %>%
+  mutate(size_mb = size / 2^20)
+> submit()
+
+| Sourcing your script...
+
+
+| You are doing so well!
+  
+  |==========================================================================================================       |  94%
+
+| A little bit more now.
+
+# Use filter() to select all rows for which size_mb is
+# less than or equal to (<=) 0.5.
+#
+# If you want your results printed to the console, add
+# print to the end of your chain.
+
+cran %>%
+  select(ip_id, country, package, size) %>%
+  mutate(size_mb = size / 2^20) %>%
+  filter(size_mb <= 0.5)
+> submit()
+
+| Sourcing your script...
+
+
+| You nailed it! Good job!
+  
+  |=============================================================================================================    |  96%
+
+| And finish it off.
+# arrange() the result by size_mb, in descending order.
+#
+# If you want your results printed to the console, add
+# print to the end of your chain.
+
+cran %>%
+  select(ip_id, country, package, size) %>%
+  mutate(size_mb = size / 2^20) %>%
+  filter(size_mb <= 0.5) %>%
+  arrange(desc(size_mb))
+
+> submit()
+
+| Sourcing your script...
+
+
+| Great job!
+  
+  |===============================================================================================================  |  98%
+
+| In this lesson, you learned about grouping and chaining using dplyr. You combined some of the things you learned in the
+| previous lesson with these more advanced ideas to produce concise, readable, and highly effective code. Welcome to the
+| wonderful world of dplyr!
